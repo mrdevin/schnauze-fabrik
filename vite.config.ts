ba57bf61c8d006a393074eff4f08933738e4ec3e
MrDevin <devin@12traits.com>
2022-07-12 00:09:03 +0200

    wip

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,23 @@
 import { defineConfig } from "vite";
+import { resolve } from 'path';
+import { fileURLToPath, URL } from 'url';
 
 // https://vitejs.dev/config/
 export default defineConfig({
     build: {
 
         rollupOptions: {
-            external: /^lit/
+            external: /^lit/,
+            input: {
+                main: resolve(__dirname, 'index.html'),
+            },
+        }
+    },
+    resolve: {
+        alias: {
+            '@': fileURLToPath(new URL('./src', import.meta.url))
         }
     }
 })
+
 
