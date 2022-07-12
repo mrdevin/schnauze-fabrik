af83079833bc8a5e1dd26cae9e7349cc479c84e9
MrDevin <devin@12traits.com>
2022-07-12 02:53:21 +0200

    favicons opengraph and twitter cards

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,36 @@
 import { defineConfig } from "vite";
 import { resolve } from 'path';
 import { fileURLToPath, URL } from 'url';
+// import favicons from 'rollup-plugin-favicons'
+// import html2 from 'rollup-plugin-html2'
 
+// import { VitePWA } from 'vite-plugin-pwa'
+// import htmlPlugin from 'vite-plugin-html-config';
+
+const htmlPluginOpt = {
+    favicon: './src/img/logo3.svg',
+
+    metas: [
+        {
+            name: 'keywords',
+            content: '3d paper wallart home decoration'
+        },
+        {
+            name: 'description',
+            content: 'Design your SPACE with 3D paper'
+        }
+    ],
+}
 // https://vitejs.dev/config/
 export default defineConfig({
     build: {
-
+        sourcemap: true,
         rollupOptions: {
-            external: /^lit/,
             input: {
                 main: resolve(__dirname, 'index.html'),
-            },
-        }
+            }
+        },
+        manifest: true
     },
     resolve: {
         alias: {
