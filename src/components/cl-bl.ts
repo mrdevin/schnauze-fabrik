10d3c1a33ecbb2755ad8c2aa23b873ad78e89bf0
MrDevin <devin@12traits.com>
2022-07-25 00:46:28 +0200

    adding observed sections

--- src/components/cl-bl.ts
+++ src/components/cl-bl.ts
@@ -1,23 +1,49 @@
 import {LitElement, html, css} from 'lit';
-import {customElement} from 'lit/decorators.js';
+import {customElement, property} from 'lit/decorators.js';
 
 @customElement('cl-bl')
 export class clBl extends LitElement {
     static styles = [
         css`
             :host {
-                display: inline;
+                display: inline-flex;
                 --clbl-size: 50px;
-                color: currentColor;
+                --clbl-color: currentColor;
+                align-items: center;
+                justify-content: center;
+            }
+
+            i{
+                display: inline;
+                font-style: normal;
+                color: var(--clbl-color);;
                 font-size: var(--clbl-size);
                 -webkit-text-stroke: 3px white;
+                margin-right: .18em;
+            }
+
+            span{
+                font-size: 1em;
+                color: currentColor
             }
         `
     ];
 
+    @property({ type: String })
+    color: string;
+
+    willUpdate(changedProperties) {
+        // console.log("🚀 ~ file: cl-bl.ts ~ line 23 ~ clBl ~ willUpdate ~ changedProperties", changedProperties, this.color)
+        // only need to check changed properties for an expensive computation.
+        if (changedProperties.has('color') && !!this.color ) {
+            this.style.setProperty('--clbl-color', this.color);
+        }
+    }
+
     render() {
         return html`
-            	⬢
+            	<i>⬢</i>
+               <span><slot></slot></span>
         `;
     }
 }
--- src/components/observed-section.ts
+++ src/components/observed-section.ts
@@ -1,5 +1,5 @@
 import {LitElement, html, css} from 'lit';
-import {customElement, property} from 'lit/decorators.js';
+import {customElement, property, state} from 'lit/decorators.js';
 
 @customElement('observed-section')
 export class LitApp extends LitElement {
@@ -7,16 +7,72 @@ export class LitApp extends LitElement {
         css`
             :host {
                 display: block;
+                min-height: 100vh;
+                box-sizing: border-box;
             }
         `
     ];
 
     @property() name = 'observed-section';
 
+    @state()
+    prevRatio = 0.0;
+
+    connectedCallback() {
+        super.connectedCallback();
+        this.createObserver(this);
+    }
+
+    increasingCallback(ratio){
+        console.log("🚀 ~ file: observed-section.ts ~ line 21 ~ LitApp ~ increasingCallback ~ ratio", ratio)
+
+    }
+
+    decreasingCallback(ratio){
+        console.log("🚀 ~ file: observed-section.ts ~ line 26 ~ LitApp ~ decreasingCallback ~ ratio", ratio)
+
+    }
+
+    handleIntersect(entries, observer) {
+        entries.forEach((entry) => {
+            if (entry.intersectionRatio > this.prevRatio) {
+                entry.target.style.backgroundColor = this.increasingCallback(entry.intersectionRatio);
+            } else {
+                entry.target.style.backgroundColor = this.decreasingCallback(entry.intersectionRatio);
+            }
+
+            this.prevRatio = entry.intersectionRatio;
+        });
+    }
+
+    createObserver(boxElement) {
+        let observer;
+
+        let options = {
+            root: null,
+            rootMargin: "0px",
+            threshold: this.buildThresholdList()
+        };
+
+        observer = new IntersectionObserver(this.handleIntersect.bind(this), options);
+        observer.observe(boxElement);
+    }
+
+    buildThresholdList() {
+        let thresholds = [];
+        let numSteps = 20;
+
+        for (let i = 1.0; i <= numSteps; i++) {
+            let ratio = i / numSteps;
+            thresholds.push(ratio);
+        }
+
+        thresholds.push(0);
+        return thresholds;
+    }
+
     render() {
         return html`
-
-            <h2>Hello, ${this.name}</h2>
             <slot></slot>
         `;
     }
