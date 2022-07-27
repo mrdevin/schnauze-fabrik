59599f360d9c81cd687e2bdca66bd2f48c474cde
MrDevin <devin@12traits.com>
2022-07-27 18:46:36 +0200

    update poster image

--- src/components/sf-model.ts
+++ src/components/sf-model.ts
@@ -1,10 +1,19 @@
-// import {LitElement, html, css} from 'lit';
+import { css} from 'lit';
 import {customElement} from 'lit/decorators.js';
 import { ModelViewerElement } from '@google/model-viewer';
 
 // @ts-ignore
 @customElement('sf-model')
 export class SfModel extends ModelViewerElement{
+
+    static styles = [
+        css`
+            :host {
+                --poster-color: transparent;
+            }
+
+        `
+    ];
     render() {
         super.render()
     }
