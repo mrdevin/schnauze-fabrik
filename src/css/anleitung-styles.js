e895e2c404e55c831d7d12a306e28b6d57012392
MrDevin <devin@12traits.com>
2022-10-23 18:40:33 +0200

    update montage index

--- src/css/anleitung-styles.js
+++ src/css/anleitung-styles.js
@@ -3,14 +3,18 @@ import { css } from 'lit';
 export const AnleitungStyles = css`
     .num,
     .note {
-        font-family: 'GT-Super-Display-Super', Rockwell, serif;
-        font-weight: bold;
+        font-family: GT-Super-Display-Super,Rockwell,serif;
+        font-weight: 700;
         font-size: 4em;
-        line-height: .7em;
         color: var(--shadow-color);
         float: left;
-        text-shadow: 1px 1px 0 var(--primary-color),6px 4px 0px  var(--highlight-color);
-        margin: 0.1em 0.15em 0em 0em;
+        text-shadow: 1px 1px 0 var(--primary-color),6px 4px 0 var(--highlight-color);
+        float: left;
+        line-height: .6em;
+        padding-top: 0px;
+        padding-bottom: 2px;
+        padding-right: 8px;
+        padding-left: 3px;
     }
 
     .note {
@@ -31,15 +35,34 @@ export const AnleitungStyles = css`
         flex-wrap: wrap;
     }
 
-    .instImg img {
+    .instImg img,
+    .icon img {
         height: 100%;
         max-width: 100%;
     }
 
+    .icon img {
+        border: unset;
+        box-shadow: unset;
+        stroke-width: 20px;
+    }
+
     .instStep p{
+        /* display: inline-flex;
+        flex-wrap: wrap; */
+        max-width: 450px;
+        width: auto;
+        margin-right: auto;
+    }
+
+    .icon {
         display: flex;
         flex-wrap: wrap;
-        max-width: 450px;
-        margin-right: auto;
+        width: 80px;
+        max-height: 80px;
+        margin: 0;
+        float: left;
+        margin-right: 1em;
+        align-content: end;
     }
 `;
--- src/pages/anleitung/anleitung-index.ts
+++ src/pages/anleitung/anleitung-index.ts
@@ -1,23 +1,43 @@
-import { LitElement, html } from 'lit';
+import { LitElement, css, html } from 'lit';
 import { customElement } from 'lit/decorators.js';
+import FlatSvg from '@/img/flat-icon.svg';
+import PointedSvg from '@/img/pointed-icon.svg';
+import { AnleitungStyles } from '/src/css/anleitung-styles.js';
 
 @customElement('anleitung-index')
 export class AnleitungIndex extends LitElement {
+    static styles = [
+        AnleitungStyles,
+        css``
+    ]
 
     createRenderRoot() { return this; }
 
     render() {
         return html`
-
+            <style>
+                ${AnleitungStyles}
+            </style>
             <h1>Anleitung</h1>
 
             <h2>Einfache Montage, Teile herausnehmen, falten,
                 kleben und auf die Wand hängen.</h2>
 
             <h3>Flache Fliese - Montageanleitung</h3>
-            <a href="/anleitung/flach-hex">zum Anleitung mit video.</a>
+            <picture class="icon">
+                <img height="253" width="400" loading="lazy" src="${FlatSvg}">
+            </picture>
+            <a href="/anleitung/flach-hex">zum Anleitung mit video.
+
+            </a>
+
             <h3>Spitzig Fliese - Montageanleitung</h3>
-            <a href="/anleitung/spitzig-hex">zum Anleitung mit video.</a>
+            <picture class="icon">
+                <img height="253" width="400" loading="lazy" src="${PointedSvg}">
+            </picture>
+            <a href="/anleitung/spitzig-hex">zum Anleitung mit video.
+
+            </a>
             <h3>Ressourcen</h3>
             <a href="/anleitung/tips">Tips Und Tricks </a>
             <br/>
