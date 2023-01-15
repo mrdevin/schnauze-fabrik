96598b882d48b86edaa3ea3a2d9d20668294c5ab
MrDevin <devin@12traits.com>
2023-01-15 17:04:01 +0100

    remove model component

--- src/pages/page-home.ts
+++ src/pages/page-home.ts
@@ -25,7 +25,6 @@ import HomeStyles from '@/css/home-styles.css?inline';
 
 import '@/components/observed-section';
 import '@/components/slide-show';
-import '@/components/sf-model';
 import '@/components/cl-bl';
 
 @customElement('page-home')
